071759921ff576ec1b639b2c01eed7727f773219
bryanagamk <bryan@kyodo-i.com>
2021-06-25 06:25:26 +0700

    Mock

---
+++ repository/category_repository_mock.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"github.com/bryanagamk/golang-unit-test/entity"
+	"github.com/stretchr/testify/mock"
+)
+
+type CategoryRepositoryMock struct {
+	Mock mock.Mock
+}
+
+func (c CategoryRepositoryMock) FindById(id string) *entity.Category {
+	arguments := c.Mock.Called(id)
+	if arguments.Get(0) == nil {
+		return nil
+	} else {
+		category := arguments.Get(0).(entity.Category)
+		return &category
+	}
+}
