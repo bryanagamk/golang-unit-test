d21a2509667669c2feaa1bd67b09a941582592be
bryanagamk <bryan@kyodo-i.com>
2021-06-25 05:25:00 +0700

    Assertion & Require

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bryanagamk/golang-unit-test
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0 // indirect
--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,23 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestHelloWoldRequire(t *testing.T) {
+	result := HelloWold("Bro")
+	require.Equal(t, "Hello Bro", result, "Result must be 'Hello Bro'")
+	fmt.Println("TestHelloWold with Require Done")
+}
+
+func TestHelloWoldAssertion(t *testing.T) {
+	result := HelloWold("Bro")
+	assert.Equal(t, "Hello Bro", result, "Result must be 'Hello Bro'")
+	fmt.Println("TestHelloWold with Assertion Done")
+}
 
 func TestHelloWold(t *testing.T) {
 	result := HelloWold("Bro")
